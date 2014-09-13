7095c5bef98eecc9214d4163086ac2d22e45ddcf
xiayun <xiayun@zxdell.com>
2014-09-13 10:10:08 +0800

    Modify: comment code which use redisclient writed by wanggang and its zxsoft company own local maven repository dependency

--- crawler-dao/src/test/java/com/zxsoft/crawler/redis/JedisTest.java
+++ crawler-dao/src/test/java/com/zxsoft/crawler/redis/JedisTest.java
@@ -1,41 +1,41 @@
-package com.zxsoft.crawler.redis;
-
-import org.junit.Test;
-
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
-
-public class JedisTest {
-
-	@Test
-	public void testJedis () throws InterruptedException {
-		JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
-		Jedis jedis = pool.getResource();
-		
-		jedis.set("foo", "bar");
-		String foobar = jedis.get("foo");
-		assert foobar.equals("bar");
-		
-		// 设置过期时间
-		jedis.set("gone", "bady, gone");
-		jedis.expire("gone", 10);
-		String there = jedis.get("gone");
-		assert there.equals("baby, gone");
-		
-		Thread.sleep(4000);
-		
-		String notThere = jedis.get("gone");
-		assert notThere == null;
-		
-		// Redis 列表
-		jedis.rpush("people", "Mary");
-		assert jedis.lindex("people", 0).equals("Mary");
-		
-		jedis.rpush("people", "Jack", "xiayun");
-		assert jedis.lindex("people", 2).equals("xiayun");
-		
-		pool.returnResource(jedis);
-		pool.destroy();
-	}
-}
+//package com.zxsoft.crawler.redis;
+//
+//import org.junit.Test;
+//
+//import redis.clients.jedis.Jedis;
+//import redis.clients.jedis.JedisPool;
+//import redis.clients.jedis.JedisPoolConfig;
+//
+//public class JedisTest {
+//
+//	@Test
+//	public void testJedis () throws InterruptedException {
+//		JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+//		Jedis jedis = pool.getResource();
+//		
+//		jedis.set("foo", "bar");
+//		String foobar = jedis.get("foo");
+//		assert foobar.equals("bar");
+//		
+//		// 设置过期时间
+//		jedis.set("gone", "bady, gone");
+//		jedis.expire("gone", 10);
+//		String there = jedis.get("gone");
+//		assert there.equals("baby, gone");
+//		
+//		Thread.sleep(4000);
+//		
+//		String notThere = jedis.get("gone");
+//		assert notThere == null;
+//		
+//		// Redis 列表
+//		jedis.rpush("people", "Mary");
+//		assert jedis.lindex("people", 0).equals("Mary");
+//		
+//		jedis.rpush("people", "Jack", "xiayun");
+//		assert jedis.lindex("people", 2).equals("xiayun");
+//		
+//		pool.returnResource(jedis);
+//		pool.destroy();
+//	}
+//}
