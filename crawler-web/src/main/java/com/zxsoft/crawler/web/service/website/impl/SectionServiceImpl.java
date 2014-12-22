2e0bf36921502344e189b145ca1ea051f57631b7
Cloud Sharl <cloudsharl@gmail.com>
2014-12-22 11:00:20 +0800

    remove last slash in section url

--- crawler-web/src/main/java/com/zxsoft/crawler/web/service/website/impl/SectionServiceImpl.java
+++ crawler-web/src/main/java/com/zxsoft/crawler/web/service/website/impl/SectionServiceImpl.java
@@ -14,46 +14,52 @@ import com.zxsoft.crawler.web.service.website.SectionService;
 @Service
 public class SectionServiceImpl implements SectionService {
 
-	@Autowired
-	private SectionDao sectionDao;
-	
-	@Override
-    public Section getSection(String sectionId) {
-		return sectionDao.getSection(sectionId);
-    }
-	
-	@Override
-	public Page<Section> getSections(Section section, int pageNo, int pageSize) {
-		return sectionDao.getSections(section, pageNo, pageSize);
-	}
+        @Autowired
+        private SectionDao sectionDao;
 
-	/**
-	 * 
+        @Override
+        public Section getSection(String sectionId) {
+                return sectionDao.getSection(sectionId);
+        }
+
+        @Override
+        public Page<Section> getSections(Section section, int pageNo, int pageSize) {
+                return sectionDao.getSections(section, pageNo, pageSize);
+        }
+
+        /**
+	 * 新增或修改版块
 	 */
-	@Override
-    public void saveOrUpdate(Section section) {
-		
-		
-		
-	    sectionDao.saveOrUpdate(section);
-    }
+        @Override
+        public void saveOrUpdate(Section section) {
 
-	@Override
-    public void delete(String id) {
-	    sectionDao.delete(id);
-	    
-    }
+                String url = section.getUrl();
+                if (url.endsWith("/")) {
+                        url = url.substring(0, url.lastIndexOf("/"));
+                }
+                section.setUrl(url);
 
-	@Override
-    public Section getSectionByUrl(String url) {
-		Section section = new Section(url);
-	    Page<Section> page = sectionDao.getSections(section, 1, 10);
-	    List<Section> sections = page.getRes();
-	    if (CollectionUtils.isEmpty(sections))
-	    	return null;
-	    return sections.get(0);
-    }
+                sectionDao.saveOrUpdate(section);
+        }
 
+        @Override
+        public void delete(String id) {
+                sectionDao.delete(id);
+        }
 
+        @Override
+        public void delete(Section section) {
+                sectionDao.delete(section);
+        }
+
+        @Override
+        public Section getSectionByUrl(String url) {
+                Section section = new Section(url);
+                Page<Section> page = sectionDao.getSections(section, 1, 10);
+                List<Section> sections = page.getRes();
+                if (CollectionUtils.isEmpty(sections))
+                        return null;
+                return sections.get(0);
+        }
 
 }
