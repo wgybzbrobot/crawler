918670c9637f352790afa454f145e6e9778b1ae9
xiayun <xia.yun@foxmail.com>
2015-02-12 10:27:39 +0800

    split img with whitespace

--- crawler-parse/src/main/java/com/zxsoft/crawler/parse/MultimediaExtractor.java
+++ crawler-parse/src/main/java/com/zxsoft/crawler/parse/MultimediaExtractor.java
@@ -1,24 +1,24 @@
 package com.zxsoft.crawler.parse;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.List;
-import java.util.Map;
+//import java.io.IOException;
+//import java.net.MalformedURLException;
+//import java.util.List;
+//import java.util.Map;
 
-import org.apache.commons.collections.map.HashedMap;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+//import org.apache.commons.collections.map.HashedMap;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+//import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+//import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+//import com.gargoylesoftware.htmlunit.WebClient;
+//import com.gargoylesoftware.htmlunit.html.HtmlElement;
+//import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.zxisl.commons.utils.CollectionUtils;
 import com.zxisl.commons.utils.StringUtils;
-import com.zxsoft.crawler.util.Utils;
+//import com.zxsoft.crawler.util.Utils;
 
 /**
  * 解析图片、音频、视频
@@ -28,58 +28,56 @@ import com.zxsoft.crawler.util.Utils;
 public class MultimediaExtractor {
 
         private static final int URL_MAX_LENGTH = 500;
-	private static WebClient webClient = new WebClient();
-	private static HtmlPage htmlPage;
+//	private static WebClient webClient = new WebClient();
+//	private static HtmlPage htmlPage;
 
-	static {
-		webClient.getOptions().setJavaScriptEnabled(true);
-		webClient.getOptions().setCssEnabled(false);
-		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-		webClient.getOptions().setTimeout(50000);
-		webClient.getOptions().setThrowExceptionOnScriptError(false);
-	}
+//	static {
+//		webClient.getOptions().setJavaScriptEnabled(true);
+//		webClient.getOptions().setCssEnabled(false);
+//		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+//		webClient.getOptions().setTimeout(50000);
+//		webClient.getOptions().setThrowExceptionOnScriptError(false);
+//	}
 
 	/**
 	 * 抽取视频URL地址
 	 * @param url
 	 */
-	public Map<String, String> extractVideo(String url) {
-		Map<String, String> videoMap = new HashedMap();
-		try {
-			htmlPage = webClient.getPage(url);
-		} catch (FailingHttpStatusCodeException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		List<HtmlElement> list = (List<HtmlElement>) htmlPage.getByXPath("//div[@class='vpic_wrap']");
-
-		if (CollectionUtils.isEmpty(list)) {
-			return null;
-		}
-		for (HtmlElement htmlElement : list) {
-
-			try {
-				htmlPage = htmlElement.click();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		}
-
-		String pageXml = htmlPage.asXml();
-		Document document = null;
-		try {
-			document = Jsoup.parse(pageXml, Utils.getHost(url));
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		String videoUrl = document.getElementsByTag("embed").first().absUrl("src");
-
-		System.out.println(videoUrl);
-		return null;
-	}
+//	public Map<String, String> extractVideo(String url) {
+//		Map<String, String> videoMap = new HashedMap();
+//		try {
+//			htmlPage = webClient.getPage(url);
+//		} catch (FailingHttpStatusCodeException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		List<HtmlElement> list = (List<HtmlElement>) htmlPage.getByXPath("//div[@class='vpic_wrap']");
+//
+//		if (CollectionUtils.isEmpty(list)) {
+//			return null;
+//		}
+//		for (HtmlElement htmlElement : list) {
+//
+//			try {
+//				htmlPage = htmlElement.click();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//
+//		}
+//
+//		String pageXml = htmlPage.asXml();
+//		Document document = null;
+//		try {
+//			document = Jsoup.parse(pageXml, Utils.getHost(url));
+//		} catch (MalformedURLException e) {
+//			e.printStackTrace();
+//		}
+//		String videoUrl = document.getElementsByTag("embed").first().absUrl("src");
+//		return null;
+//	}
 
 	/**
 	 * 抽取图片url, url.length < 500
@@ -96,14 +94,14 @@ public class MultimediaExtractor {
 				if (imgUrlSb.length() + src.length() > URL_MAX_LENGTH ) 
 				        break;
 				if (!src.matches(urlfiter))
-					imgUrlSb.append(src);
+					imgUrlSb.append(" " + src);
 			}
 		} else {
 			for (Element img : imgs) {
 			        String src = img.attr("abs:src");
 			        if (imgUrlSb.length() + src.length()> URL_MAX_LENGTH ) 
                                         break;
-				imgUrlSb.append(src);
+				imgUrlSb.append(" " + src);
 			}
 		}
 		return imgUrlSb.toString();
