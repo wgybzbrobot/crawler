4cdca60a451104d3f93b68b815900b60d0550f82
夏云 <xiayun@zxisl.com>
2015-03-12 16:27:23 +0800

    匹配bz=0的tid

--- crawler-master/src/main/java/crawler/data/SiteIdTransfer.java
+++ crawler-master/src/main/java/crawler/data/SiteIdTransfer.java
@@ -5,9 +5,11 @@ import java.io.InputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import oracle.jdbc.driver.OracleDriver;
 
@@ -74,13 +76,12 @@ public class SiteIdTransfer {
                 return oracleJdbcTemplate;
         }
         
-        public static void main(String[] args) {
+        
+        public static void importTid(JdbcTemplate mysqlJdbcTemplate, JdbcTemplate oracleJdbcTemplate, String category, int num ) {
 
-                JdbcTemplate mysqlJdbcTemplate = getMysqlJdbcTemplate();
-                JdbcTemplate oracleJdbcTemplate = getOracleJdbcTemplate();
-                
                 List<Website> list = mysqlJdbcTemplate.query("select a.id, a.site, a.comment, a.tid "
-                                                + " from website a, section b where a.id = b.site and b.category = 'forum' ",
+                                                + " from website a, section b where a.id = b.site and b.category = ? ",
+                                                new Object[]{category},
                         new RowMapper<Website>() {
                                 public Website mapRow(ResultSet rs, int rowNum) throws SQLException {
                                         return new Website( rs.getInt("id"),   rs.getString("site"),  rs.getString("comment"), rs.getInt("tid"));
@@ -92,8 +93,8 @@ public class SiteIdTransfer {
                                 continue;
                         
                         List<Map<String, Object>> _list = oracleJdbcTemplate.query("select id, zdmc from fllb_cjlb "
-                                                        + " where zdmc=? and zdbs=2 and shzt=1 and bz=1 ", 
-                                                        new Object[]{website.getComment()}, new RowMapper<Map<String, Object>>() {
+                                                        + " where zdmc=? and zdbs=? and shzt=1 and bz=0 ", 
+                                                        new Object[]{website.getComment(), num}, new RowMapper<Map<String, Object>>() {
                                 public Map<String, Object> mapRow(ResultSet rs, int arg1) throws SQLException {
                                         Map<String, Object> map = new HashMap<String, Object>();
                                         map.put("id", rs.getString("id"));
@@ -111,8 +112,115 @@ public class SiteIdTransfer {
                         mysqlJdbcTemplate.update("update website set tid= ? where id = ? ", new Object[]{_map.get("id"), website.getId()});
                         
                 }
+        }
+
+        /**
+         * mysql有, oracle没有的网站
+         * @param mysqlJdbcTemplate
+         * @param oracleJdbcTemplate
+         */
+        public static int mysqlHas(JdbcTemplate mysqlJdbcTemplate, JdbcTemplate oracleJdbcTemplate, String category, int num ) {
+                List<Website> list = mysqlJdbcTemplate.query("select a.id, a.site, a.comment, a.tid "
+                                                + " from website a, section b where a.id = b.site and b.category = ? and a.tid is null ",
+                                                new Object[]{category},
+                        new RowMapper<Website>() {
+                                public Website mapRow(ResultSet rs, int rowNum) throws SQLException {
+                                        return new Website( rs.getInt("id"),   rs.getString("site"),  rs.getString("comment"), rs.getInt("tid"));
+                                }
+                });
+                LOG.info("mysql 有但oracle没有的站点:" + category);
+                Set<String> set = new HashSet<String>();
                 
+                for (Website website : list) {
+                        if (website.getTid() != 0) 
+                                continue;
+                        
+                        List<Map<String, Object>> _list = oracleJdbcTemplate.query("select id, zdmc from fllb_cjlb "
+                                                        + " where zdmc=? and zdbs=? and shzt=0  ", 
+                                                        new Object[]{website.getComment(), num}, new RowMapper<Map<String, Object>>() {
+                                public Map<String, Object> mapRow(ResultSet rs, int arg1) throws SQLException {
+                                        Map<String, Object> map = new HashMap<String, Object>();
+                                        map.put("id", rs.getString("id"));
+                                        map.put("zdmc", rs.getString("zdmc"));
+                                        return map;
+                                }
+                        });
+                        
+                        if (CollectionUtils.isEmpty(_list)) {
+                                set.add(website.getComment() + "\t\t\t" + website.getSite());
+                        } 
+                }
+                System.out.println("size:" + set.size());
+                for (String string : set) {
+                       System.out.println(string);
+                }
+                return set.size();
+        }
+
+        /**
+         * oracle 有,但mysql没有
+         * @param mysqlJdbcTemplate
+         * @param oracleJdbcTemplate
+         */
+        public static int oracleHas(JdbcTemplate mysqlJdbcTemplate, JdbcTemplate oracleJdbcTemplate, String category, int num ) {
+                List<Website> list = oracleJdbcTemplate.query("select id, zdmc, zdzy from fllb_cjlb "
+                                                + " where   zdbs=? and shzt=1  ", new Object[]{num},  new RowMapper<Website>() {
+                        public Website mapRow(ResultSet rs, int arg1) throws SQLException {
+                                Website website = new Website();
+                                website.setId(rs.getInt("id"));
+                                website.setComment(rs.getString("zdmc"));
+                                website.setSite(rs.getString("zdzy"));
+                                return website;
+                        }
+                });
                 
+
+                LOG.info("oracle 有但mysql没有的站点" + category);
+                Set<String> set = new HashSet<String>();
+                
+                for (Website website : list) {
+                        List<Website> _list = mysqlJdbcTemplate.query("select a.id, a.site, a.comment, a.tid "
+                                                        + " from website a, section b where a.id = b.site and b.category = ? and a.comment=?", 
+                                                        new Object[]{category, website.getComment()},
+                                new RowMapper<Website>() {
+                                        public Website mapRow(ResultSet rs, int rowNum) throws SQLException {
+                                                return new Website( rs.getInt("id"),   rs.getString("site"),  rs.getString("comment"), rs.getInt("tid"));
+                                        }
+                        });
+                        
+                        if (CollectionUtils.isEmpty(_list)) {
+                                set.add(website.getComment() + "\t\t\t" + website.getSite());
+                        }
+                }
+                System.out.println("size:" + set.size());
+                for (String string : set) {
+                       System.out.println(string);
+                }
+                return  set.size();
+        }
+        
+        
+        public static void main(String[] args) {
+                JdbcTemplate mysqlJdbcTemplate = getMysqlJdbcTemplate();
+                JdbcTemplate oracleJdbcTemplate = getOracleJdbcTemplate();
+                
+                Map<String, Integer> categories = new HashMap<String, Integer>();
+                categories.put("news", 1);
+                categories.put("forum", 2);
+//                categories.put("news", 3);
+                categories.put("blog", 4);
+//                categories.put("news", 5);
+                categories.put("search", 6);
+                
+                int sum = 0;
+                for (String key : categories.keySet()) {
+                        int i = categories.get(key);
+//                        sum += oracleHas(mysqlJdbcTemplate, oracleJdbcTemplate,key, i);
+                sum += mysqlHas(mysqlJdbcTemplate, oracleJdbcTemplate, key, i);
+//                importTid(mysqlJdbcTemplate, oracleJdbcTemplate, key, i);
+                        
+                }
+                System.out.println("sum" + sum);
                 
         }
 
